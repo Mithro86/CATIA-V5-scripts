9549508edc71efd7046c5d48050f3704777b09e4
Mithro86 <84014236+Mithro86@users.noreply.github.com>
2022-12-21 15:53:57 +0100

    Add files via upload
    Translate bodies and geometrical sets using googletrans.

---
+++ catpart_lang_translator.py
@@ -0,0 +1,146 @@
+# required googletrans==4.0.0-rc1
+
+import os
+import sys
+from googletrans import Translator
+
+translator = Translator()
+
+sys.path.insert(0, os.path.abspath("..\\pycatia"))
+
+from pycatia import catia
+
+caa = catia()
+document = caa.active_document
+part = document.part
+bodies = part.bodies
+hybrid_bodies = part.hybrid_bodies
+
+languages = {
+    "af": "afrikaans",
+    "sq": "albanian",
+    "am": "amharic",
+    "ar": "arabic",
+    "hy": "armenian",
+    "az": "azerbaijani",
+    "eu": "basque",
+    "be": "belarusian",
+    "bn": "bengali",
+    "bs": "bosnian",
+    "bg": "bulgarian",
+    "ca": "catalan",
+    "ceb": "cebuano",
+    "ny": "chichewa",
+    "zh-cn": "chinese (simplified)",
+    "zh-tw": "chinese (traditional)",
+    "co": "corsican",
+    "hr": "croatian",
+    "cs": "czech",
+    "da": "danish",
+    "nl": "dutch",
+    "en": "english",
+    "eo": "esperanto",
+    "et": "estonian",
+    "tl": "filipino",
+    "fi": "finnish",
+    "fr": "french",
+    "fy": "frisian",
+    "gl": "galician",
+    "ka": "georgian",
+    "de": "german",
+    "el": "greek",
+    "gu": "gujarati",
+    "ht": "haitian creole",
+    "ha": "hausa",
+    "haw": "hawaiian",
+    "iw": "hebrew",
+    "he": "hebrew",
+    "hi": "hindi",
+    "hmn": "hmong",
+    "hu": "hungarian",
+    "is": "icelandic",
+    "ig": "igbo",
+    "id": "indonesian",
+    "ga": "irish",
+    "it": "italian",
+    "ja": "japanese",
+    "jw": "javanese",
+    "kn": "kannada",
+    "kk": "kazakh",
+    "km": "khmer",
+    "ko": "korean",
+    "ku": "kurdish (kurmanji)",
+    "ky": "kyrgyz",
+    "lo": "lao",
+    "la": "latin",
+    "lv": "latvian",
+    "lt": "lithuanian",
+    "lb": "luxembourgish",
+    "mk": "macedonian",
+    "mg": "malagasy",
+    "ms": "malay",
+    "ml": "malayalam",
+    "mt": "maltese",
+    "mi": "maori",
+    "mr": "marathi",
+    "mn": "mongolian",
+    "my": "myanmar (burmese)",
+    "ne": "nepali",
+    "no": "norwegian",
+    "or": "odia",
+    "ps": "pashto",
+    "fa": "persian",
+    "pl": "polish",
+    "pt": "portuguese",
+    "pa": "punjabi",
+    "ro": "romanian",
+    "ru": "russian",
+    "sm": "samoan",
+    "gd": "scots gaelic",
+    "sr": "serbian",
+    "st": "sesotho",
+    "sn": "shona",
+    "sd": "sindhi",
+    "si": "sinhala",
+    "sk": "slovak",
+    "sl": "slovenian",
+    "so": "somali",
+    "es": "spanish",
+    "su": "sundanese",
+    "sw": "swahili",
+    "sv": "swedish",
+    "tg": "tajik",
+    "ta": "tamil",
+    "te": "telugu",
+    "th": "thai",
+    "tr": "turkish",
+    "uk": "ukrainian",
+    "ur": "urdu",
+    "ug": "uyghur",
+    "uz": "uzbek",
+    "vi": "vietnamese",
+    "cy": "welsh",
+    "xh": "xhosa",
+    "yi": "yiddish",
+    "yo": "yoruba",
+    "zu": "zulu",
+}
+
+# set destination language
+dest_language = "en"
+
+# translate body names
+for body in bodies:
+    translation = translator.translate(body.name, dest=dest_language)
+    body.name = translation.text
+
+# translate geometrical set names (two levels deep)
+for hb in hybrid_bodies:
+    translation = translator.translate(hb.name, dest=dest_language)
+    hb.name = translation.text
+
+    lvl2 = hb.hybrid_bodies
+
+    for hb2 in lvl2:
+        translation = translator.translate(hb2.name, dest=dest_language)
+        hb2.name = translation.text
